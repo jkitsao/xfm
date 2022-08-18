a0fa81e89628d6b672d2ac828dda2f93d7df9142
kitsao <kitsaojackson22@gmail.com>
2022-08-18 12:57:50 +0300

    increase nav logo text font-size

--- components/navigation/comp/Navbar.jsx
+++ components/navigation/comp/Navbar.jsx
@@ -6,7 +6,7 @@ function Navbar() {
         <nav className='sticky top-0 z-50 border-b-8 bg-black  border-red-800/40'>
             <section className='w-full flex justify-center text-center py-1'>
                 <div>
-                    <h2 className='p-2 text-3xl lg:text-6xl font-bold  font-bree text-red-700 flex justify-center items-center'>
+                    <h2 className='p-2 text-5xl lg:text-6xl font-bold  font-bree text-red-700 flex justify-center items-center'>
                         {/* <Image src='/assets/rockforever.webp' width={50} height={50} classNamemx-2 alt='' /> */}
                         <span className='mx-3 underline-offset-1'>
                             Xfm online
