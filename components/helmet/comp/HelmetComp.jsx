cad41d8f535b2f42fa8b10b7044cf061e00d26ff
kitsao <kitsaojackson22@gmail.com>
2022-11-15 22:30:06 +0300

    new title tagline

--- components/helmet/comp/HelmetComp.jsx
+++ components/helmet/comp/HelmetComp.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Head from 'next/head';
 function HelmetComp() {
+    // Rock out with the best online rock station!
     const title='Listen to Great Rock music online'
     return (
         <Head>
@@ -8,18 +9,18 @@ function HelmetComp() {
             <title>{title && title}</title>
             <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
             <meta name='title' content={title && title} />
-            <meta name="description" content="Home of Great rock music" />
+            <meta name="description" content="Rock out with the best Online Rock music station!" />
             {/* Open Graph / Facebook */}
             <meta property="og:type" content="website" />
             <meta property="og:url" content="https://xfmradio.co.ke/" />
             <meta property="og:title" content={title && title} />
-            <meta property="og:description" content='Home of Great rock music' />
+            <meta property="og:description" content='Rock out with the best Online Rock music station!' />
             <meta property="og:image" content="/assets/rockstar.jpg" />
             {/* Twitter */}
             <meta property="twitter:card" content="summary_large_image" />
             <meta property="twitter:url" content="https://xfmradio.co.ke/" />
             <meta property="twitter:title" content={title && title} />
-            <meta property="twitter:description" content="Home of Great rock music" />
+            <meta property="twitter:description" content="Rock out with the best Online Rock music station!" />
             <meta property="twitter:image" content='/assets/rockstar.jpg' />
         </Head>
 
