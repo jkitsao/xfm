b8bf5ec66203e6fb9e0fb2f41111a47710ee5458
kitsao <kitsaojackson22@gmail.com>
2022-11-17 11:45:34 +0300

    fix chat system not working

--- pages/api/createTokenRequest.js
+++ pages/api/createTokenRequest.js
@@ -1,7 +1,9 @@
 import Ably from "ably/promises";
-
+const clientId = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
 export default async function handler(req, res) {
     const client = new Ably.Realtime(process.env.ABLY_API_KEY);
-    const tokenRequestData = await client.auth.createTokenRequest({ clientId: 'ably-nextjs-demo' });
-    res.status(200).json(tokenRequestData);
+    const tokenRequestData = await client.auth.createTokenRequest({ clientId: 'radio-chat-app' });
+    console.log(tokenRequestData)
+    if(tokenRequestData) return res.status(200).json(tokenRequestData);
+    return res.status(200).json({message:'bad request'});
 };
