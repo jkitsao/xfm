017b161ddec17a23b0e8e5699640824278aab952
kitsao <kitsaojackson22@gmail.com>
2022-11-23 16:37:26 +0300

    fix meta image property

--- components/helmet/comp/HelmetComp.jsx
+++ components/helmet/comp/HelmetComp.jsx
@@ -15,13 +15,13 @@ function HelmetComp() {
             <meta property="og:url" content="https://xfmradio.co.ke/" />
             <meta property="og:title" content={title && title} />
             <meta property="og:description" content='Rock out with the best Online Rock and Alternative music station!' />
-            <meta property="og:image" content="/assets/rockstar.jpg" />
+            <meta property="og:image" content="https://xfmradio.co.ke/assets/rockstar.jpg" />
             {/* Twitter */}
             <meta property="twitter:card" content="summary_large_image" />
             <meta property="twitter:url" content="https://xfmradio.co.ke/" />
             <meta property="twitter:title" content={title && title} />
             <meta property="twitter:description" content="Rock out with the best Online Rock and Alternative music station!" />
-            <meta property="twitter:image" content='/assets/rockstar.jpg' />
+            <meta property="twitter:image" content='https://xfmradio.co.ke/assets/rockstar.jpg' />
         </Head>
     )
 }
