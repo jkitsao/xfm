0e1c7e9adc06daf3761239ad79fd50659ce48bb6
kitsao <kitsaojackson22@gmail.com>
2023-05-10 00:17:32 +0300

    feat: add download feature

--- components/request/RequestModal.js
+++ components/request/RequestModal.js
@@ -1,28 +1,28 @@
-import React,{useState} from 'react'
-import { Modal, Button, Group } from '@mantine/core';
+import React, { useState } from "react";
+import { Modal, Button, Group } from "@mantine/core";
+import Link from "next/link";
 function RequestModal() {
-    const [opened, setOpened] = useState(false);
+  const [opened, setOpened] = useState(false);
   return (
     <>
-      <Modal
+      {/* <Modal
         opened={opened}
         onClose={() => setOpened(false)}
         title="Find and Request your favourite song"
         size='xl'
       >
         {/* Modal content */}
-        {/* <AblyChatComponent receivedMessages={receivedMessages} setMessages={setMessages} channel={channel} ably={ably}/> */}
+      {/* <AblyChatComponent receivedMessages={receivedMessages} setMessages={setMessages} channel={channel} ably={ably}/> 
        <iframe src="https://radio.xfmradio.co.ke/public/xfmonline/embed-requests?theme=dark" frameBorder={0} allowTransparency="true" style={{width: '100%', minHeight: 550, border: 0}} />
-      </Modal>
-        {/* <Button onClick={() => setOpened(true)}></Button> */}
-        <section className='w-full py-1  flex justify-center items-center'>
-        <button onClick={() => setOpened(true)} className='border-2 text-white border-red-700 bg-red-600/20 py-3 px-10 rounded-3xl text-sm '>
-       Request a Song
+      </Modal> */}
+      {/* <Button onClick={() => setOpened(true)}></Button> */}
+      <section className="w-full py-1  flex justify-center items-center">
+        <button className="border-2 text-white border-red-700 bg-red-600/20 py-3 px-10 rounded-3xl text-sm ">
+          <Link href="/playlist">Download from Playlist</Link>
         </button>
-        </section>
-      
+      </section>
     </>
-  )
+  );
 }
 
-export default RequestModal
+export default RequestModal;
--- pages/playlist.js
+++ pages/playlist.js
@@ -2,14 +2,14 @@ import React from "react";
 
 function catalogue() {
   return (
-    <>
+    <div className="p-2">
       <iframe
         src="https://dashboard.xfmradio.co.ke/public/xfm-online/ondemand/embed?theme=dark"
         frameBorder={0}
         allowTransparency="true"
-        style={{ width: "100%", minHeight: 400, border: 0 }}
+        style={{ width: "100%", minHeight: 750, border: 0 }}
       />
-    </>
+    </div>
   );
 }
 
