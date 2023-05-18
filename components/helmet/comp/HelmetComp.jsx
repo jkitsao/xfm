543599f7153f05e2006f40d4e8763274ab541715
kitsao <kitsaojackson22@gmail.com>
2023-05-18 14:59:58 +0300

    fix: resolve OG tag issue

--- components/helmet/comp/HelmetComp.jsx
+++ components/helmet/comp/HelmetComp.jsx
@@ -21,27 +21,27 @@ function HelmetComp({data}) {
     //   })();
     // }, []);
     const title='Listen to Great Rock and Alternative hits online'
-   const dynamic_og_image= `https://xfmradio.co.ke/api/og?title=${data?.text}&artist=${data?.artist}&art=${data?.art}`
+//    const dynamic_og_image= `https://xfmradio.co.ke/api/og?title=${data?.text}&artist=${data?.artist}&art=${data?.art}`
     return (
         <Head>
             {/* Primary Meta Tags */}
-            <title>{data?.artist && data?.artist}</title>
+            <title>Now Playing {data?.text}</title>
             <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
             <meta name='title' content={title && title} />
             <meta name="description" content="Rock out with the best Online Rock and Alternative music station!" />
-            <meta property="og:image" content={dynamic_og_image}/>
+            <meta property="og:image" content={data?.art}/>
             {/* Open Graph / Facebook */}
             <meta property="og:type" content="website" />
             <meta property="og:url" content="https://xfmradio.co.ke/" />
             <meta property="og:title" content={title && title} />
             <meta property="og:description" content='Rock out with the best Online Rock and Alternative music station!' />
-            <meta property="og:image" content={dynamic_og_image} />
+            <meta property="og:image" content={data?.art} />
             {/* Twitter */}
             <meta property="twitter:card" content="summary_large_image" />
             <meta property="twitter:url" content="https://xfmradio.co.ke/" />
             <meta property="twitter:title" content={title && title} />
             <meta property="twitter:description" content="Rock out with the best Online Rock and Alternative music station!" />
-            <meta property="twitter:image" content={dynamic_og_image} />
+            <meta property="twitter:image" content={data?.art} />
         </Head>
     )
 }
