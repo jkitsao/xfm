f7a510fe217dc43a5f46ea2741fdf8c2681baa49
Jackson kitsao <kitsaojackson22@gmail.com>
2024-03-30 14:56:43 +0300

    fix: update email banner endpoint

--- components/navigation/comp/Navbar.jsx
+++ components/navigation/comp/Navbar.jsx
@@ -8,7 +8,7 @@ function Navbar() {
         <img
           src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTLZt4v_gnup86P48zzfOn5gqio0K-u6-5Xuzln4szl0A&s"
           alt="logo"
-          className="w-12 h-12 rounded shadow-xl shadow-red-700 animate-pulse object-contain "
+          className="w-12 h-12 rounded shadow-xl shadow-red-700 animate-pulse object-contain mr-2"
         />
         <span className="text-md text-center leading-tight ">
           Rock and Alternative Music 24/7
