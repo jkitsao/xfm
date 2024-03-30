f4b829b3f87a39f695cbc4ed94902213d2557f75
Jackson kitsao <kitsaojackson22@gmail.com>
2024-03-30 14:53:20 +0300

    fix: update email banner endpoint

--- components/navigation/comp/Navbar.jsx
+++ components/navigation/comp/Navbar.jsx
@@ -4,13 +4,15 @@ import Image from "next/image";
 function Navbar() {
   return (
     <nav className="lg:py-3  bg-clip-padding backdrop-filter backdrop-blur-xl bg-opacity-60   ">
-      <div className="flex justify-center items-center p-5 text-xl font-semibold text-white">
+      <div className="flex justify-center items-center p-3 lg:p-5 text-xl font-semibold text-300-700/80">
         <img
           src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTLZt4v_gnup86P48zzfOn5gqio0K-u6-5Xuzln4szl0A&s"
           alt="logo"
-          className="w-12 h-12 object-contain mx-2"
+          className="w-12 h-12 rounded shadow-xl shadow-red-700 animate-pulse object-contain "
         />
-        <span className="text-center">Rock and Alternative Music 24/7</span>
+        <span className="text-md text-center leading-tight ">
+          Rock and Alternative Music 24/7
+        </span>
       </div>
     </nav>
   );
