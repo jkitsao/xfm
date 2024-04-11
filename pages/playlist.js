d1fa82159b6566470aab0851305eaa4990911a9a
Jackson kitsao <kitsaojackson22@gmail.com>
2024-04-11 04:01:39 +0300

    feat: add ondemand page

--- pages/playlist.js
+++ pages/playlist.js
@@ -1,10 +1,12 @@
 import React from "react";
+import HelmetComp from "../components/helmet";
 
-function catalogue() {
+export default function catalogue({ data }) {
   return (
-    <div className="p-2">
+    <div className="">
+      <HelmetComp data={data} />
       <iframe
-        src="https://dashboard.xfmradio.co.ke/public/xfm-online/ondemand/embed?theme=dark"
+        src="https://studio.xfm.co.ke/public/xfm.co.ke/ondemand/embed?theme=dark"
         frameBorder={0}
         allowTransparency="true"
         style={{ width: "100%", minHeight: 750, border: 0 }}
@@ -13,4 +15,18 @@ function catalogue() {
   );
 }
 
-export default catalogue;
+// This gets called on every request
+export async function getServerSideProps() {
+  // Fetch data from external API
+  try {
+    const res = await fetch(`https://studio.xfm.co.ke/api/nowplaying/xfm`);
+    const data = await res.json();
+    // Pass data to the page via props
+    console.log({ nowplaying: data[0].now_playing.song });
+    return { props: { data: data[0].now_playing.song } };
+  } catch (error) {
+    console.error(error);
+    return error;
+  }
+  // console.log(data);
+}
